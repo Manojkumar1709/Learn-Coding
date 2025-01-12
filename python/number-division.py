f8c9f5e23cf26a634f6cba71e05f7adfa8bb7142
Manojkumar1709 <mm2040922@gmail.com>
2025-01-12 13:05:20 +0530

    updated

--- python/number-division.py
+++ python/number-division.py
@@ -1,8 +1,8 @@
 
-#Write a Python program that prints the numbers from 1 to 100 and checks their divisibility with all numbers between 2 and 100.
-#  a. For each number from 1 to 100, the program should check if the number is divisible by any of the numbers from 2 to 100.
-#  b. If a number is divisible by one or more numbers, the program should print the divisors. If it is not divisible by any 
-#     of the numbers from 2 to 100, print a message stating that it is not divisible by any of the specified numbers.
+#  Write a Python program that prints the numbers from 1 to 100 and checks their divisibility with all numbers between 2 and 100.
+#    a. For each number from 1 to 100, the program should check if the number is divisible by any of the numbers from 2 to 100.
+#    b. If a number is divisible by one or more numbers, the program should print the divisors. If it is not divisible by any 
+#       of the numbers from 2 to 100, print a message stating that it is not divisible by any of the specified numbers.
 
 
 print("Number Division")
